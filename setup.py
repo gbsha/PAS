c88efc8df6381ded3167d215e745d32e73ca5dc2
Elysia Love <elysia@love>
2024-03-24 11:54:09 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pycm",
+    version="0.1",
+    author="Georg Boecherer",
+    author_email="mail@georg-boecherer.de",
+    packages=find_packages(include="pycm"),
+    url="https://github.com/gbsha/PAS",
+    install_requires=['numpy >= 1.11.1'],
+)
